49c0e65872e7d11a1f18d3de3cc7b7b3e7c3d61c
Леонид <lenya.zverevich@gmail.com>
2024-06-03 20:23:35 +0300

    Added homework

--- lection_2/homework.js
+++ lection_2/homework.js
@@ -1,54 +1,56 @@
-//Как исправить "одни пятёрки"?
-
-// var result = [];
-// for (var i = 0; i < 5; i++) {
-//     result[i] = function () {
-//         console.log(i);
-//     };
-// }
-// result[0](); //5
-// result[1](); //5
-// result[2](); //5
-// result[3](); //5
-// result[4](); //5
+var result = [];
+for (let i = 0; i < 5; i++) {
+  result[i] = function () {
+    console.log(i);
+  };
+}
+result[0]();
+result[1]();
+result[2]();
+result[3]();
+result[4]();
 
 //////////////////////////////////////////////////
 
-// function getGroup() {
-//     let students = [];
-//     let i = 0;
-//     while (i < 10) {
-//         students[i] = function() {
-//             console.log(i);
-//         }
-//         i++
-//     }
-//
-//     return students;
-// }
-//
-// let group = getGroup();
-//
-// group[0](); // 10 как исправить на 0
-// group[5](); // 10                  5
+function getGroup() {
+  let students = [];
+  let i = 0;
+  while (i < 10) {
+    let j = i;
+    students[j] = function () {
+      console.log(j);
+    };
+    i++;
+  }
+  return students;
+}
+
+let group = getGroup();
+
+group[0]();
+group[5]();
 
 //////////////////////////////////////////////////
 
 // Напишите функцию multiply, должна принимать произвольное количество аргументов и возвращать их произведение.
 
-// const result1 = multiply(2)(3)(4);
-// console.log(result1); // Вывод: 24
-// const result2 = multiply(2)(3)(4)(5);
-// console.log(result2); // Вывод: 120
+function multiply(a) {
+  let sum = a;
+  function innerMultiply(b) {
+    if (b == undefined) {
+      return a;
+    } else {
+      sum = a * b;
+      return multiply(sum);
+    }
+  }
+  return innerMultiply;
+}
 
-// const result1 = multiply(2)(3)(4);
-//
-// // Пример использования:
-// const result1 = multiply(2)(4);
-// console.log(result1); // Вывод: 8
-//
-// const result2 = multiply(5)(2)(3);
-// console.log(result2); // Вывод: 30
+const result1 = multiply(2)(3)(4)();
+console.log(result1); // Вывод: 24
+const result2 = multiply(2)(3)(4)(5)();
+console.log(result2); // Вывод: 120
 
 /////////////////////////
 // Написать функцию getUniqArray(arr), которая на вход принимает массив чисел и
@@ -57,3 +59,18 @@
 //     Текст ошибки: "В getUniqArray был передан невалидный параметр. Аргумент arr
 // должен быть массивом чисел".
 
+function getUniqArray(array) {
+  let uniqNumbers = [];
+  for (let number of array) {
+    if (typeof number !== "number")
+      throw new Error(
+        "В getUniqArray был передан невалидный параметр. Аргумент arr должен быть массивом чисел"
+      );
+    if (!uniqNumbers.includes(number)) uniqNumbers.push(number);
+  }
+  return uniqNumbers;
+}
+console.log(getUniqArray([1, 1, 1, 5, 5]));
+console.log(getUniqArray([1, 2, 2, 3, 4, 5]));
+// console.log(getUniqArray([1, 2, 2, 3, 4, 5, "5"]));
+// console.log(getUniqArray(["5", "2"]));
